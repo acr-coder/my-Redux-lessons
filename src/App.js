ba99151993e0d808777245740a532d9b9c576349
acr-coder <acarerisa1@gmail.com>
2021-06-07 14:55:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { createStore } from 'redux';
 import './App.css';
+
+let initialState = {
+  elma : 0
+}
+
+const elmaReducer = (state = initialState, action) => {
+  switch(action.type){
+    case "EKLE":
+      return {elma: state.elma + action.payload}
+    case "CIKART":
+      return {elma: state.elma - action.payload}
+    default:
+      return state
+  }
+}
+
+const store = createStore(elmaReducer)
+store.subscribe(()=> console.log("Store değişti ",store.getState()))
+
+store.dispatch({type:"EKLE", payload:3})
+//console.log(store.getState())
+
+store.dispatch({type:"EKLE", payload:5})
+//console.log(store.getState())
+
+store.dispatch({type:"CIKART", payload:2})
+//console.log(store.getState())
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello redux
       </header>
     </div>
   );
